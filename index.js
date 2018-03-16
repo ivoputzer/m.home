4b7e463655daf01377761f136a488c426dbfca5a
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2018-03-16 14:26:18 +0100

    fixes standard format

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const {normalize, resolve} = require('path')
 const $home = process.env.HOME || process.env.USERPROFILE
 
-function home(){
+function home () {
   let home = process.env.HOME || process.env.USERPROFILE
-  if (!home)
-    throw new Error('could not find a valid user home path')
+  if (!home) throw new Error('could not find a valid user home path')
   return normalize(resolve(home, ...arguments))
 }
 
